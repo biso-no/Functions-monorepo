16afa012d479b0b6bb3a582e016ef90328e5e6d1
MHEien <markushei@hotmail.no>
2024-07-28 01:32:39 +0200

    Update chatGroup.ts

--- packages/appwrite/src/chatGroup.ts
+++ packages/appwrite/src/chatGroup.ts
@@ -12,7 +12,11 @@ export async function createTeam(jwt: string, teamName: string, users?: Models.D
     const team = await teams.create(teamId, teamName);
     
     if (team.$id) {
-        const teamDocument = await databases.createDocument('app', 'chats', teamId, { name: teamName }, [
+        const teamDocument = await databases.updateDocument('app', 'user', user.$id, { chats: [
+            {
+            name: teamName
+            }
+        ] }, [
             Permission.read(Role.team(teamId)),
             Permission.update(Role.team(teamId)),
             Permission.delete(Role.user(user.$id)),
@@ -24,17 +28,12 @@ export async function createTeam(jwt: string, teamName: string, users?: Models.D
         if (users) {
             for (const user of users) {
                 await teams.createMembership(teamId, [], "", user.$id, undefined, "https://669544b0281f8e2277e6.appwrite.biso.no/");
-                await databases.updateDocument('app', 'user', user.$id, { chats: [teamId] });
             }
         }
 
         if (emails) {
             for (const email of emails) {
                 const membership = await teams.createMembership(teamId, [], email, undefined, undefined, "https://669544b0281f8e2277e6.appwrite.biso.no/");
-                if (membership.$id) {
-                    const userDocument = await databases.createDocument('app', 'user', membership.userId, { email: email });
-                    await databases.updateDocument('app', 'user', userDocument.$id, { chats: [teamId] });
-                }
             }
         }
     }
