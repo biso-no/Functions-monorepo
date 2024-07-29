28083f9a8110701bfb2cb663e6afd19b3a7b343b
MHEien <markushei@hotmail.no>
2024-07-29 22:31:54 +0200

    Vipps checkout

---
+++ functions/vipps-payment/index.ts
@@ -0,0 +1,56 @@
+import { createCheckout } from "../../packages/vipps/src/index.js";
+import { createSessionClient, ID } from "@biso/appwrite";
+
+type Context = {
+    req: any;
+    res: any;
+    log: (msg: any) => void;
+    error: (msg: any) => void;
+};
+
+export default async ({ req, res, log, error }: Context) => {
+log('On Vipps Payment POST request');
+    const { amount, description, returnUrl,  callbackAuthorizationToken } = req.body;
+
+    log('Parsed request body: ' + JSON.stringify(req.body));
+    if (!amount || !description || !returnUrl ) {
+        log('Missing required parameters');
+        return res.json({ error: 'Missing required parameters' });
+    }
+
+    const reference = ID.unique();
+
+    try {
+    const checkout = await createCheckout({
+        reference,
+        amount,
+        description,
+        returnUrl,
+    });
+
+    if (checkout.ok) {
+
+        log('Checkout created: ' + JSON.stringify(checkout));
+        log('Initiating Admin Client...');
+        const { databases } = await createSessionClient(req.headers['x-appwrite-user-jwt'] as string);
+
+        log('Creating checkout document...');
+
+        const doc = await databases.createDocument('app', 'checkout', reference, {
+            reference,
+            amount,
+            description,
+        });
+    
+        log('Checkout document created: ' + JSON.stringify(doc));
+
+        return res.json({ checkout });
+      } else {
+        log('Error initiating checkout:' + checkout.error);
+        return res.json({ error: 'Failed to initiate checkout' });
+      }
+} catch (error) {
+    log('Error initiating checkout:' + error);
+    return res.json({ error: 'Failed to initiate checkout' });
+  }
+}
---
+++ packages/vipps/src/index.ts
@@ -0,0 +1,48 @@
+import { Client } from "@vippsmobilepay/sdk";
+import { ID } from "node-appwrite";
+
+const merchantSerialNumber = process.env.MERCHANT_SERIAL_NUMBER!;
+const subscriptionKey = process.env.SUBSCRIPTION_KEY!;
+const clientId = process.env.CLIENT_ID!;
+const clientSecret = process.env.CLIENT_SECRET!;
+
+const client = Client({
+    merchantSerialNumber,
+    subscriptionKey,
+    useTestMode: process.env.VIPPS_TEST_MODE ? true : false,
+    retryRequests: false,
+  });
+
+
+export const accessToken = await client.auth.getToken(clientId, clientSecret);
+
+export async function createCheckout({
+    reference,
+    amount,
+    description,
+    returnUrl,
+}: {
+    reference: string,
+    amount: number,
+    description: string,
+    returnUrl: string,
+}) {
+  
+    const checkout = await client.checkout.create(clientId, clientSecret, {
+        merchantInfo: {
+          callbackUrl: process.env.VIPPS_CALLBACK_URL!,
+          returnUrl: returnUrl,
+          callbackAuthorizationToken: ID.unique(),
+        },
+        transaction: {
+        reference: reference,
+          amount: {
+            currency: 'NOK',
+            value: amount, 
+          },
+          paymentDescription: description
+        },
+      });
+
+      return checkout;
+    }
