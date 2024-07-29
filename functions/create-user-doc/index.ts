597948248c77b741e09b5d8836bfcce0af5fa2db
MHEien <markushei@hotmail.no>
2024-07-29 15:37:25 +0200

    Update index.ts

--- functions/create-user-doc/index.ts
+++ functions/create-user-doc/index.ts
@@ -14,44 +14,38 @@ interface UserDoc {
 
 export default async ({ req, res, log, error }: Context) => {
     try {
-        log('Starting function execution');
-
+        
         const { userId, email } = req.body as UserDoc;
-        log('Parsed post data: ' + JSON.stringify(req.body));
+        log('UserID: ' + userId);
+        log('Email: ' + email);
 
-        //Path to image url: "yoast_head_json.schema.@graph[2].url"
+        if (!userId || !email) {
+            log('No userId or email provided');
+            return res.json({ error: 'No userId or email provided' });
+        }
+
         log('Creating session client');
         const { databases } = await createSessionClient(req.headers['x-appwrite-user-jwt']!);
         log('Session client created');
 
-        if (userId && email) {
-            log('Checking for existing document');
-            const existingDoc = await databases.getDocument('app', 'user', userId, [
-                Query.equal('email', email),
-                Query.select(['email', '$id'])
-            ]);
-            log('Existing document check complete: ' + JSON.stringify(existingDoc));
-            
-            if (existingDoc.$id) {
-                log('Document already exists');
-                return res.json({ status: 'ok', exists: true });
-            }
-
-            log('Creating new user document');
-            const userDoc = await databases.createDocument('app', 'user', userId, {
-                email
-            });
-            log('Created user document: ' + JSON.stringify(userDoc));
-
-            log('Retrieving user document');
-            const user = await databases.getDocument('app', 'user', userDoc.$id);
-            log('Retrieved user document: ' + JSON.stringify(user));
-
-            return res.json({ status: 'ok', exists: false });
-        } else if (!email && !userId) {
-            log('No email or ID provided');
-            return res.json({ error: 'No email or ID provided' });
+        const existingDoc = await databases.getDocument('app', 'user', userId, [
+            Query.equal('email', email),
+            Query.select(['email', '$id'])
+        ]);
+        log('Existing document check complete: ' + JSON.stringify(existingDoc));
+        
+        if (existingDoc.$id) {
+            log('Document already exists');
+            return res.json({ status: 'ok', exists: true });
         }
+
+        log('Creating new user document');
+        const userDoc = await databases.createDocument('app', 'user', userId, {
+            email
+        });
+        log('Created user document: ' + JSON.stringify(userDoc));
+
+        return res.json({ status: 'ok', exists: false });
     } catch (err: any) {
         error('An error occurred: ' + (err as Error).message);
         return res.json({ error: 'An internal error occurred' });
