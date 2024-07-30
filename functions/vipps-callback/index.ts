cea2f837019f135d4a4a38f024ecf5b64ea2765b
MHEien <markushei@hotmail.no>
2024-07-30 03:06:49 +0200

    Update index.ts

--- functions/vipps-callback/index.ts
+++ functions/vipps-callback/index.ts
@@ -57,44 +57,43 @@ export default async ({ req, res, log, error }: Context) => {
 
     log('Retreiving access token...');
     const token = await getAccessToken();
-    if (token.ok) {
-        log('Access token fetched: ' + JSON.stringify(token));
-
-        const { reference, name, amount, success } = body;
-        log('Parsed request body: ' + JSON.stringify(body));
-
-        if (!reference || !name || !amount || !success) {
-            log('Missing required parameters');
-            return res.json({ error: 'Missing required parameters' });
-        }
-
-        const payment = await getPayment({
-            reference,
-            token: token.data.access_token,
-        });
-        const { databases } = await createAdminClient();
-        if (payment.ok) {
-            log('Payment found: ' + JSON.stringify(payment));
-
-            const paymentDoc = await databases.updateDocument('app', 'payments', reference, {
-                status: success ? 'SUCCESS' : 'FAILED',
-                paid_amount: success ? amount.value : 0,
-                payment_method: success ? payment.data.paymentMethod : null,
-            });
-            log('Payment document updated: ' + JSON.stringify(paymentDoc));
-            return res.json({ payment });
-        } else {
-            log('Error getting payment:' + payment.error);
-            const paymentDoc = await databases.updateDocument('app', 'payment', reference, {
-                status: 'failed',
-                paid_amount: 0,
-                payment_method: 'unknown',
-            });
-            log('Payment document updated: ' + JSON.stringify(paymentDoc));
-            return res.json({ payment });
-        }
-    } else {
+    if (!token.ok) {
         log('Error fetching access token: ' + token.error);
         return res.json({ error: 'Failed to fetch access token' });
     }
+
+    const { reference, name, amount, success } = body;
+    log('Parsed request body: ' + JSON.stringify(body));
+
+    if (!reference || !name || !amount || !success) {
+        log('Missing required parameters');
+        return res.json({ error: 'Missing required parameters' });
+    }
+
+    const payment = await getPayment({
+        reference,
+        token: token.data.access_token,
+    });
+    const { databases } = await createAdminClient();
+    if (!payment.ok) {
+        log('Error getting payment:' + payment.error);
+        const paymentDoc = await databases.updateDocument('app', 'payment', reference, {
+            status: 'failed',
+            paid_amount: 0,
+            payment_method: 'unknown',
+        });
+        log('Payment document updated: ' + JSON.stringify(paymentDoc));
+        return res.json({ payment });
+    }
+
+    log('Payment found: ' + JSON.stringify(payment));
+
+    const paymentDoc = await databases.updateDocument('app', 'payments', reference, {
+        status: success ? 'SUCCESS' : 'FAILED',
+        paid_amount: success ? amount.value : 0,
+        payment_method: payment.data.paymentMethod
+    });
+    log('Payment document updated: ' + JSON.stringify(paymentDoc));
+    return res.json({ payment });   
+
 }
