73a59c5131e598b2987b563e0f4df6e69be4bb83
MHEien <markushei@hotmail.no>
2024-08-12 13:53:05 +0200

    Update index.ts

--- functions/create-membership-from-shop/index.ts
+++ functions/create-membership-from-shop/index.ts
@@ -9,25 +9,21 @@ type Context = {
 };
 
 interface RequestBody {
-    customer: string;
+    customer: any;
     custom_field_value: string;
     selected_variation: string;
     price: string;
 }
 
-const SHOULD_INVOICE = process.env.SHOULD_INVOICE!
-
-const SHOULD_CREATE_CUSTOMER = process.env.SHOULD_CREATE_CUSTOMER!
-
-
+const SHOULD_INVOICE = process.env.SHOULD_INVOICE!;
+const SHOULD_CREATE_CUSTOMER = process.env.SHOULD_CREATE_CUSTOMER!;
 
 export default async ({ req, res, log, error }: Context) => {
+    try {
         log('Parsing request body...');
         log('Request body: ' + JSON.stringify(req.body));
 
-        // Use the req.body directly as it is already an object
         const body = req.body as RequestBody;
-
         const { customer, custom_field_value: student_id, selected_variation, price } = body;
 
         log(`Parsed request body: ${JSON.stringify(body)}`);
@@ -36,30 +32,38 @@ export default async ({ req, res, log, error }: Context) => {
             error('Missing required parameters: customer, student_id, or selected_variation');
             return res.json({ error: 'Missing required parameters' });
         }
-        
-        const shouldCreateCustomer = SHOULD_CREATE_CUSTOMER === 'true';
 
+        const shouldCreateCustomer = SHOULD_CREATE_CUSTOMER === 'true';
         let existingCustomer: Customer | null = null;
         const { createInvoice, getAccessToken, updateCustomerCategory, getCustomer, createCustomer } = soapClient(log, error);
+
         const { accessToken, status: tokenStatus } = await getAccessToken();
         if (tokenStatus !== 'ok') {
             log('Failed to retrieve access token');
             return res.json({ error: 'Failed to retrieve access token' });
         }
         log('Token response: ' + JSON.stringify(accessToken));
+
         const studentId = parseInt(student_id.replace('s', ''));
-        const response = await getCustomer(accessToken, studentId);
-        log('Response: ' + JSON.stringify(response));
+        let response;
+        try {
+            response = await getCustomer(accessToken, studentId);
+            log('Response: ' + JSON.stringify(response));
+        } catch (err) {
+            error(`Error during customer retrieval: ${err}`);
+            return res.json({ error: 'Failed to retrieve customer' });
+        }
+
         if (response) {
-            existingCustomer = await response as Customer;
+            existingCustomer = response as Customer;
             log(`Existing customer found for user_id: ${student_id} - ${JSON.stringify(existingCustomer)}`);
-        } else if (SHOULD_CREATE_CUSTOMER === 'true') {
+        } else if (shouldCreateCustomer) {
             log(`Customer not found for user_id: ${student_id}, creating new customer...`);
-            const parsedCustomer = JSON.parse(customer);
             const customerResponse = await createCustomer(accessToken, {
                 $id: student_id,
-                name: parsedCustomer.firstName + ' ' + parsedCustomer.lastName,
-                firstName: parsedCustomer.firstName,
+                name: `${customer.first_name} ${customer.last_name}`,
+                firstName: customer.first_name,
+                lastName: customer.last_name,
             });
 
             if (!customerResponse) {
@@ -69,42 +73,19 @@ export default async ({ req, res, log, error }: Context) => {
 
             existingCustomer = await customerResponse.json() as Customer;
             log(`New customer created with ID: ${existingCustomer.Id} for user_id: ${studentId}`);
-        } 
-        if (!SHOULD_CREATE_CUSTOMER && !existingCustomer) {
-            const parsedCustomer = JSON.parse(customer);
+        }
+
+        if (!shouldCreateCustomer && !existingCustomer) {
             log(`Customer not found for user_id: ${student_id}, and SHOULD_CREATE_CUSTOMER is false. Exiting early...`);
-            //Get the name of the membership type based on the selected_variation
             const membershipType = campusMapping[selected_variation].type;
             const status = "Mottatt";
-            await sendStatusUpdateToSharepoint(studentId, parsedCustomer.name, membershipType.toString(), status, log, error);
+            await sendStatusUpdateToSharepoint(studentId, `${customer.first_name} ${customer.last_name}`, membershipType.toString(), status, log, error);
             return res.status(404).json({ error: 'Customer not found and creation is disabled' });
         }
 
         const campus = determineCampusId(selected_variation);
-        
-
         const departmentId = determineDepartmentId(campus.campus_id);
         const accrualDate = determineAccrualDate();
-
-
-        // Check if membership is a string and parse it, otherwise assume it's already an object
-        let customerObj: any;
-        if (typeof customer === 'string') {
-            try {
-                customerObj = JSON.parse(customer);
-            } catch (parseError) {
-                log('Failed to parse membership string');
-                return res.json({ error: 'Invalid membership format' });
-            }
-        } else {
-            customerObj = customer;
-        }
-
-        // Ensure membershipObj has the expected properties
-        if (!customerObj.category || !customerObj.name) {
-            log('Membership object is missing required properties');
-            return res.json({ error: 'Invalid membership object' });
-        }
 
         const categoryMapping: { [key: string]: number } = {
             '22141': 113170, // Semester
@@ -120,58 +101,67 @@ export default async ({ req, res, log, error }: Context) => {
             '22151': 113171,
             '22152': 113171,
         };
-        
 
         const membershipObj = {
-            customerId: customerObj.customerId,
-            membershipId: customerObj.membershipId,
-            category: categoryMapping[selected_variation], // Category based on variation
-            name: campusMapping[selected_variation].type,  // Use the type from campusMapping to represent the membership type
+            customerId: existingCustomer?.Id || studentId,
+            membershipId: selected_variation,
+            category: categoryMapping[selected_variation],
+            name: campusMapping[selected_variation].type,
         };
-        
 
-        const customerCategoryId = await updateCustomerCategory(accessToken, membershipObj.category, studentId);
+        try {
+            await updateCustomerCategory(accessToken, membershipObj.category, studentId);
+        } catch (err) {
+            log('Error updating customer category');
+            return res.json({ error: 'Failed to update customer category' });
+        }
 
         const invoiceStatus = SHOULD_INVOICE === 'true' ? 'Invoiced' : 'Draft';
         const accrualLength = determineAccrualLength(campusMapping[selected_variation].type);
 
         const userCampus = {
             campus: determineCampusId(selected_variation),
-            name: customerObj.firstName + ' ' + customerObj.lastName,
+            name: `${customer.first_name} ${customer.last_name}`,
         };
 
-        const invoiceResponse = await createInvoice(accessToken, {
-            CustomerId: studentId,
-            OrderStatus: invoiceStatus,
-            DepartmentId: departmentId,
-            IncludeVAT: true,
-            PaymentAmount: parseFloat(price),
-            PaymentMethodId: 1,
-            PaymentTime: 0,
-            UserDefinedDimensions: [
-                {
-                    Type: UserDefinedDimensionKey.UserDefined,
-                    Name: userCampus.name,
-                    TypeId: '101',
-                },
-                {
-                    Type: UserDefinedDimensionKey.UserDefined,
-                    Name: membershipObj.name,
-                    TypeId: '102',
-                },
-            ],
-            InvoiceRows: [
-                {
-                    ProductId: parseInt(membershipObj.membershipId),
-                    Price: parseFloat(price),
-                    Quantity: 1,
-                },
-            ],
-            AccrualDate: accrualDate,
-            AccrualLength: accrualLength,
-        });
-        
-        log('Invoice response: ' + JSON.stringify(invoiceResponse));
+        let invoiceResponse;
+        try {
+            invoiceResponse = await createInvoice(accessToken, {
+                CustomerId: studentId,
+                OrderStatus: invoiceStatus,
+                DepartmentId: departmentId,
+                IncludeVAT: true,
+                PaymentAmount: parseFloat(price),
+                PaymentMethodId: 1,
+                PaymentTime: 0,
+                UserDefinedDimensions: [
+                    {
+                        Type: UserDefinedDimensionKey.UserDefined,
+                        Name: userCampus.name,
+                        TypeId: '101',
+                    },
+                    {
+                        Type: UserDefinedDimensionKey.UserDefined,
+                        Name: membershipObj.name,
+                        TypeId: '102',
+                    },
+                ],
+                InvoiceRows: [
+                    {
+                        ProductId: parseInt(membershipObj.membershipId),
+                        Price: parseFloat(price),
+                        Quantity: 1,
+                    },
+                ],
+                AccrualDate: accrualDate,
+                AccrualLength: accrualLength,
+            });
+            log('Invoice response: ' + JSON.stringify(invoiceResponse));
+        } catch (err) {
+            log('Error creating invoice');
+            return res.json({ error: 'Failed to create invoice' });
+        }
+
         if (invoiceResponse && existingCustomer && membershipObj) {
             const name = existingCustomer.Name;
             const membershipType = membershipObj.category;
@@ -179,8 +169,12 @@ export default async ({ req, res, log, error }: Context) => {
 
             await sendStatusUpdateToSharepoint(studentId, name, membershipType.toString(), status, log, error);
         }
-        return res.json({ temp: 'temp' });
+        return res.json({ success: 'Process completed successfully' });
+    } catch (err) {
+        error(`Unexpected error: ${err instanceof Error ? err.message : 'Unknown error'}`);
+        return res.status(500).json({ error: 'An unexpected error occurred' });
     }
+};
 
 
 // Utility function to determine department ID based on campus ID
