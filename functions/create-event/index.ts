a07e96568d57b35bde21280a5d2172ee757a05e4
MHEien <markushei@hotmail.no>
2024-08-28 01:07:23 +0200

    Update index.ts

--- functions/create-event/index.ts
+++ functions/create-event/index.ts
@@ -1,103 +1,29 @@
 import { createAdminClient } from "@biso/appwrite";
 
-type Context = {
+interface Event {
+    event: string;
+    data: {
+        id: number;
+        date: string;
+        slug: string;
+        status: string;
+        link: string;
+        title: {
+            rendered: string;
+        };
+        content: {
+            rendered: string;
+        };
+    }
+    organizer_name: string;
+}
+
+interface Context {
     req: any;
     res: any;
     log: (msg: any) => void;
     error: (msg: any) => void;
 };
-
-interface Post {
-    id: number;
-    date: string;
-    link: string;
-    slug: string;
-    status: string;
-    title: Content;
-    content: Content;
-    acf: Acf;
-}
-
-interface Content {
-    rendered: string;
-}
-
-interface Acf {
-    campus: {
-        value: string;
-        label: string;
-    };
-    department_oslo?: {
-        value: string;
-        label: string;
-    };
-    department_bergen?: {
-        value: string;
-        label: string;
-    };
-    department_stavanger?: {
-        value: string;
-        label: string;
-    };
-    department_trondheim?: {
-        value: string;
-        label: string;
-    };
-    department_national?: {
-        value: string;
-        label: string;
-    }
-}
-
-
-export default async ({ req, res, log, error }: Context) => {
-
-    log('Request received' + JSON.stringify(req.body));
-
-    if (req.method === 'GET') {
-        return res.json({ message: 'Not allowed.' });
-    }
-
-    const { 
-        id, 
-        title, 
-        content, 
-        start_date, 
-        end_date, 
-        location, 
-        venue,
-        organizer,
-        cost,
-        url,
-        primary_image: image
-    } = req.body;
-    
-    if (req.method === 'POST') {
-    const client = createAdminClient();
-
-    try {
-        const event = (await client).databases.createDocument('app', 'events', id, {
-            id: id,
-            date: start_date,
-            link: location,
-            slug: id,
-            status: 'published',
-            title,
-            description: content,
-            campus: getCampusIdFromOrganizerSlug(organizer),
-            campus_id: getCampusIdFromOrganizerSlug(organizer),
-            price: cost,
-            url
-        });
-        log('Event created' + JSON.stringify(event));
-        return res.json(event);
-    } catch (error) {
-        log('Error occurred: ' + JSON.stringify(error));
-        return res.json({ message: 'Internal server error' });
-    }
-}
-};
-
 function getCampusIdFromOrganizerSlug(slug: string) {
     const campusMapping = {
         'biso-oslo': 1,
@@ -109,4 +35,73 @@ function getCampusIdFromOrganizerSlug(slug: string) {
     
     // Return the corresponding campus ID or undefined if the slug doesn't match
     return campusMapping[slug as keyof typeof campusMapping];
+}
+export default async ({ req, res, log, error }: Context) => {
+
+    log('Request received' + JSON.stringify(req.body));
+
+    if (req.method === 'GET') {
+        return res.json({ message: 'Not allowed.' });
+    }
+
+    const { 
+        event,
+        data: {
+            id,
+            date,
+            slug,
+            status,
+            link,
+            title,
+            content,
+        },
+        organizer_name
+    } = req.body as Event;
+
+    if (event === 'event_created') {
+        const client = createAdminClient();
+
+        try {
+            const event = (await client).databases.createDocument('app', 'events', id.toString(), {
+                title: title.rendered,
+                description: content.rendered,
+                event_date: date,
+                campus_id: getCampusIdFromOrganizerSlug(organizer_name),
+                campus: getCampusIdFromOrganizerSlug(organizer_name),
+                url: link,
+                slug,
+                status
+            }
+
+            );
+
+            return res.json({ message: 'Event created successfully' });
+        } catch (error) {
+            return res.status(500).json({ message: 'Event creation failed', error });
+        }
+    }
+
+    if (event === 'event_updated') {
+        const client = createAdminClient();
+
+        try {
+            const event = (await client).databases.updateDocument('app', 'events', id.toString(), {
+                title: title.rendered,
+                description: content.rendered,
+                event_date: date,
+                campus_id: getCampusIdFromOrganizerSlug(organizer_name),
+                campus: getCampusIdFromOrganizerSlug(organizer_name),
+                url: link,
+                slug,
+                status
+            });
+
+            return res.json({ message: 'Event updated successfully' });
+        } catch (error) {
+            return res.status(500).json({ message: 'Event update failed', error });
+        }
+    }
+
+    res.json({ message: 'Event not found' });
+
 }
