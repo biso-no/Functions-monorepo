bdc6c703f459c5c25199fe584774016a283f2cad
DanielleRaposonAjoc <danielleajoc123@gmail.com>
2025-02-26 10:49:26 +0100

    -generate-invoice

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -30,3 +30,91 @@ export interface Product {
     products: Product[];
   }
   
+  export interface ExpenseAttachment {
+    date: string;
+    url: string;
+    amount: number;
+    description: string;
+    type: string;
+    $id: string;
+    $createdAt: string;
+    $updatedAt: string;
+    $permissions: string[];
+    $databaseId: string;
+    $collectionId: string;
+  }
+  
+  export interface StudentId {
+    student_id: string;
+    expiry_date: string | null;
+    isMember: boolean;
+    $id: string;
+    $createdAt: string;
+    $updatedAt: string;
+    $permissions: string[];
+    $databaseId: string;
+    $collectionId: string;
+  }
+  
+  export interface Campus {
+    name: string;
+    $id: string;
+    $createdAt: string;
+    $updatedAt: string;
+    $permissions: string[];
+    $databaseId: string;
+    $collectionId: string;
+  }
+  
+  export interface Department {
+    Id: string;
+    Name: string;
+    campus_id: string;
+    $id: string;
+    $createdAt: string;
+    $updatedAt: string;
+    $permissions: string[];
+    $databaseId: string;
+    $collectionId: string;
+  }
+  
+  export interface User {
+    phone: string;
+    address: string;
+    city: string;
+    zip: string;
+    bank_account: string;
+    name: string;
+    email: string | null;
+    $id: string;
+    $createdAt: string;
+    $updatedAt: string;
+    $permissions: string[];
+    studentId: StudentId;
+    campus: Campus;
+    chats: any[];
+    departments: Department[];
+    $databaseId: string;
+    $collectionId: string;
+  }
+  
+  export interface Expense {
+    bank_account: string;
+    campus: string;
+    department: string;
+    description: string;
+    prepayment_amount: number;
+    total: number;
+    status: string;
+    userId: string;
+    $id: string;
+    $permissions: string[];
+    $createdAt: string;
+    $updatedAt: string;
+    expenseAttachments: ExpenseAttachment[];
+    user: User;
+    invoice_id: string | null;
+    $databaseId: string;
+    $collectionId: string;
+  }
+  
